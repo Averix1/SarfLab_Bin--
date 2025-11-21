a7daf32df66d4f425e8ee2fd77f41e821973b57a
Averix1 <yusufdoyar20@gmail.com>
2025-11-21 19:21:25 +0300

    js/app.js

---
+++ js/app.js
@@ -0,0 +1,67 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 20px;
+    background-color: #f9f9f9;
+    color: #2c3e50;
+    text-align: center;
+}
+
+/* Header */
+header {
+    font-size: 28px;
+    font-weight: bold;
+    margin-bottom: 30px;
+}
+
+/* Splash ekranı */
+#splash {
+    position: fixed;
+    top:0;
+    left:0;
+    width: 100%;
+    height: 100%;
+    background-color: #f9f9f9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 50px;
+    font-weight: bold;
+    color: #2c3e50;
+    z-index: 9999;
+}
+
+#sarf {
+    display: inline-block;
+    transition: transform 1s ease, opacity 1s ease;
+}
+
+#bina {
+    display: inline-block;
+    opacity: 0;
+    margin-left: 20px;
+    transition: opacity 1s ease;
+}
+
+/* Arama kutusu */
+.search-box input {
+    width: 60%;
+    max-width: 400px;
+    padding: 12px 15px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    margin-bottom: 20px;
+}
+
+/* PWA Butonu */
+#installBtn {
+    cursor: pointer;
+}
+
+/* Kelime sonucu */
+#wordBox {
+    margin-top: 20px;
+    font-size: 18px;
+    color: #e74c3c;
+}
